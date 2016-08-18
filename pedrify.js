03c2af8b7dbe856d514a44eaf01110a2e5a3e2b2
Nebil Kawas <nkawas90@gmail.com>
2016-08-18 14:22:29 -0300

    :sparkles: Added a function to replace a specific character

--- pedrify.js
+++ pedrify.js
@@ -7,7 +7,28 @@
         outputBox.innerHTML = inputBox.pedrify();
     });
 
+    String.prototype.replaceBy = function(character, index) {
+        return this.slice(0, index) + character + this.slice(index + 1);
+    };
+
     String.prototype.pedrify = function() {
-        // TODO
+        var REPLACE = {
+            'b': 'v',
+            'v': 'b',
+            'z': 's',
+            'á': 'a',
+            'é': 'e',
+            'í': 'i',
+            'ó': 'o',
+            'ú': 'u',
+        };
+
+        var output = this;
+        for (var index = 0; index < output.length; index++) {
+            var character = output.slice(index, index + 1);
+            output = output.replaceBy(REPLACE[character] || character, index);
+        }
+
+        return output;
     };
 }());
